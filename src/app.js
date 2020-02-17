6ad078a2ac37b06570d7d03a264bce2c44b92f29
Wanderson Luan <luanpinhoo@gmail.com>
2020-02-17 14:05:19 -0400

    feat(Dev): added initial config the server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import routes from './routes'
+
+class App {
+    constructor() {
+        this.server = express()
+
+        // chamando meus métodos
+        this.middlewares() 
+        this.routes()
+    }
+
+    middlewares() { // Registrando meus middlewares
+        this.server.use(express.json())
+    }
+
+    routes() {  // Registrando minhas rotas
+        this.server.use(routes)
+    }
+}
+
+export default new App().server
